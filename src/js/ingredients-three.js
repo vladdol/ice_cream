cea455478f263d8035bca2481b4322463928bc18
vladdol <109621867+vladdol@users.noreply.github.com>
2023-01-05 10:45:54 +0200

    add

---
+++ src/js/ingredients-three.js
@@ -0,0 +1,14 @@
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[data-ingredients-three-open]'),
+    closeModalBtn: document.querySelector('[data-ingredients-close]'),
+    modal: document.querySelector('[data-ingredients]'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+  }
+})();
